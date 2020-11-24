3adfbb5ef22d902229b5313113e6ded9ad1ef9e6
Andrej-oss <snowtomsawyer@gmail.com>
2020-11-25 00:57:58 +0100

    -addGuardLink(admin, user)

---
+++ src/app/logic/guard-url/user-guard.service/user-guard.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {UserService} from '../../services/post.service/user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuardService implements CanActivate{
+
+  constructor(private userService: UserService,
+              private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.userService.isUser()){
+      return of(true);
+    }
+    else {
+      this.router.navigate(['authenticate'], {
+        queryParams: {
+          accessDenied: true
+        }
+      }).then(data => console.log(data));
+    }
+    return of(false);
+  }
+}
